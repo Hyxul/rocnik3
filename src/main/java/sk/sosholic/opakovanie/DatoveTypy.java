8007675db88e9e6f0879f92b09b5b1dd96a5739b
Holy <hyxulcrsean@gmail.com>
2022-09-14 11:23:48 +0200

    Initial commit

---
+++ src/main/java/sk/sosholic/opakovanie/DatoveTypy.java
@@ -0,0 +1,13 @@
+package sk.sosholic.opakovanie;
+
+public class DatoveTypy {
+	public static void main(String[] args) {
+		System.out.println("byte uchovava prirodzene cislo velkosti 8b");
+		System.out.println("short uchovava prirodzene cislo velkosti 16b");
+		System.out.println("char uchovava string velkosti 16b");
+		System.out.println("int uchovava prirodzene cislo velkosti 32b");
+		System.out.println("float uchovava celé aj desatinné cislo velkosti 32b");
+		System.out.println("double uchovava prirodzene cislo velkosti 64b");
+		System.out.println("boolean dáva hodnoty true/false");
+	}
+}
---
+++ src/main/java/sk/sosholic/opakovanie/cvicenie.java
@@ -0,0 +1,8 @@
+package sk.sosholic.opakovanie;
+
+public class cvicenie {
+	public static void main(String[] args) {
+
+		System.out.println("Cez leto som robil autoškolu" + "\n"+ "Chodieval som von s kamarátmi");
+	}
+}
